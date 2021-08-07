72b33190a9dda075489bcdab78c9c03c0be86a2a
Jon Borichevskiy <jborichevskiy@gmail.com>
2021-08-07 10:16:23 -0700

    Deploy Next.js to gh-pages

--- _next/static/3l1tD-tTJC4PGPmV-8OhF/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-7bff40bcbdd63e00eeb9.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
